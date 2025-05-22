d4d96913e893bc2de2f3e3f2b4eb21707ad9c33c
Nishant kumar <nishantkumarsingh946@gmail.com>
2025-05-22 21:08:33 +0530

    Refactor attendance management pages: remove unused components and consolidate attendance functionality
    - Deleted Attendance, MarkAttendance, MarkTeacherAttendance, StudentReport, TeacherAttendance, TodayAttendance, and TodayTeacherAttendance components.
    - Updated imports and removed unnecessary code to streamline attendance management.
    - Improved code organization and readability by removing redundant imports and unused variables.

--- Pahal/src/pages/attendance/todayattendance.tsx
+++ Pahal/src/pages/attendance/todayattendance.tsx
@@ -9,7 +9,7 @@ import { useAttendance } from '@/contexts/attendanceContext'
 function TodayAttendance() {
   const navigate = useNavigate()
   const [searchQuery, setSearchQuery] = useState('');
-  const { todayAttendance, unmarkStudentAttendance } = useAttendance();
+  const { todayAttendance } = useAttendance();
   const [currentDate] = useState(new Date().toLocaleDateString('en-US', {
     weekday: 'long',
     year: 'numeric',
