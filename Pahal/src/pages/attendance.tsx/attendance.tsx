47239516307087320f5a4b07003b7598e89e2771
Nishant kumar <nishantkumarsingh946@gmail.com>
2025-05-22 21:00:35 +0530

    fix: remove unused React import in attendance and teacher attendance components

--- Pahal/src/pages/attendance.tsx/attendance.tsx
+++ Pahal/src/pages/attendance.tsx/attendance.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Layout from '@/components/layout/layout'
 import { Link } from 'react-router-dom'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
--- Pahal/src/pages/attendance.tsx/teacherattendance.tsx
+++ Pahal/src/pages/attendance.tsx/teacherattendance.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Layout from '@/components/layout/layout'
 import { Button } from '@/components/ui/button'
