50ee22889941a79afa862092dea1590a2ca58e4f
Somesh Mishra <someshmishra3436@gmail.com>
2025-06-28 02:05:07 +0530

    Refactor server routes and remove unused files
    - Updated CORS middleware to allow specific origins.
    - Cleaned up formatting in index.js for consistency.
    - Removed unused admin and teacher route files.
    - Refactored authRoutes.js for improved readability by standardizing import and route definitions.

--- server/routes/v1/authRoutes.js
+++ server/routes/v1/authRoutes.js
@@ -1,18 +1,9 @@
-import express, { Router } from 'express';
-import { loginUser, registerUser } from '../../controllers/authController.js';
+import express, { Router } from "express";
+import { loginUser, registerUser } from "../../controllers/authController.js";
 
 const authRoutes = Router();
 
-authRoutes.post('/login',loginUser);
-authRoutes.post('/register',registerUser);
+authRoutes.post("/login", loginUser);
+authRoutes.post("/register", registerUser);
 
 export default authRoutes;
-
-/*
-    Types of requests:
-    1. GET: Retrieve data from the server.
-    2. POST: Send data to the server to create a new resource.
-    3. PUT: Update an existing resource on the server.
-    4. DELETE: Remove a resource from the server.
-    5. PATCH: Partially update an existing resource on the server.
-*/
